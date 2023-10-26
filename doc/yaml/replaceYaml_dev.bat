f8808e04b4cbd71ae22d647a380e7bac16bc5a22
Dabu9 <1948614228@qq.com>
2023-10-26 17:00:44 +0800

    <add>: 添加本地部署功能

---
+++ doc/yaml/replaceYaml_dev.bat
@@ -0,0 +1,42 @@
+echo #######################
+echo ##### begin copy ######
+echo #######################
+
+echo ###### copy action_admin ########
+copy .\conf\dev\action_admin\application.yml ..\..\action_admin\src\main\resources\
+copy .\conf\dev\action_admin\bootstrap.yml ..\..\action_admin\src\main\resources\
+
+
+echo ###### copy action_gateway ########
+copy .\conf\dev\action_gateway\application.yml ..\..\action_gateway\src\main\resources\
+copy .\conf\dev\action_gateway\bootstrap.yml ..\..\action_gateway\src\main\resources\
+
+
+echo ###### copy action_monitor ########
+copy .\conf\dev\action_monitor\application.yml ..\..\action_monitor\src\main\resources\
+copy .\conf\dev\action_monitor\bootstrap.yml ..\..\action_monitor\src\main\resources\
+
+
+echo ###### copy action_picture ########
+copy .\conf\dev\action_picture\application.yml ..\..\action_picture\src\main\resources\
+copy .\conf\dev\action_picture\bootstrap.yml ..\..\action_picture\src\main\resources\
+
+
+echo ###### copy action_search ########
+copy .\conf\dev\action_search\application.yml ..\..\action_search\src\main\resources\
+copy .\conf\dev\action_search\bootstrap.yml ..\..\action_search\src\main\resources\
+
+
+echo ###### copy action_sms ########
+copy .\conf\dev\action_sms\application.yml ..\..\action_sms\src\main\resources\
+copy .\conf\dev\action_sms\bootstrap.yml ..\..\action_sms\src\main\resources\
+
+
+echo ###### copy action_spider ########
+copy .\conf\dev\action_spider\application.yml ..\..\action_spider\src\main\resources\
+copy .\conf\dev\action_spider\bootstrap.yml ..\..\action_spider\src\main\resources\
+
+
+echo ###### copy action_web ########
+copy .\conf\dev\action_web\application.yml ..\..\action_web\src\main\resources\
+copy .\conf\dev\action_web\bootstrap.yml ..\..\action_web\src\main\resources\
